4e67ce85c7a54933e9df7ec0cb6bfd6169a248f7
Tatsuya Sato <satoryu.1981@gmail.com>
2019-07-14 22:28:30 +0900

    Generate all stuff with bundle gem command

---
+++ lib/rubocop/too_short_instance_variable.rb
@@ -0,0 +1,8 @@
+require "rubocop/too_short_instance_variable/version"
+
+module Rubocop
+  module TooShortInstanceVariable
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/rubocop/too_short_instance_variable_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Rubocop::TooShortInstanceVariable do
+  it "has a version number" do
+    expect(Rubocop::TooShortInstanceVariable::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
