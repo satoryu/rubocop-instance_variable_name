55f63eddd44e05fa26c719a5671feb2c63c87aa2
Tatsuya Sato <satoryu.1981@gmail.com>
2019-07-15 10:30:42 +0900

    Refactoring

--- lib/rubocop/cop/naming/too_short_instance_variable.rb
+++ lib/rubocop/cop/naming/too_short_instance_variable.rb
@@ -5,11 +5,18 @@ module RuboCop
     module Naming
       class TooShortInstanceVariable < ::RuboCop::Cop::Cop
         MSG = 'Instance variable `%<ivar_name>s` is too short.'.freeze
+        DEFAULT_ACCEPTABLE_LENGTH = 2.freeze
 
         def on_ivasgn(node)
           ivar_name, _ = *node
-          add_offense(node, message: format(MSG, ivar_name: ivar_name)) if ivar_name.length <= 3
+          var_name = ivar_name[/^@(\w+)/, 1]
+          add_offense(node, message: format(MSG, ivar_name: ivar_name)) if var_name.length <= acceptable_length
         end
+
+        private
+          def acceptable_length
+            cop_config['AcceptableLength'] || DEFAULT_ACCEPTABLE_LENGTH
+          end
       end
     end
   end
--- spec/rubocop/cop/naming/too_short_instance_variable_spec.rb
+++ spec/rubocop/cop/naming/too_short_instance_variable_spec.rb
@@ -1,11 +1,21 @@
 require 'spec_helper'
 
 RSpec.describe RuboCop::Cop::Naming::TooShortInstanceVariable do
-  subject(:cop) { described_class.new }
-  it '' do
-    expect_offense <<~CODE
-      @hg = 1
-      ^^^^^^^ Instance variable `@hg` is too short.
-    CODE
+  let(:config) { RuboCop::Config.new }
+  subject(:cop) { described_class.new(config) }
+
+  context 'In default,' do
+    specify 'instance variable name with length 2 is marked as offensive' do
+      expect_offense <<~CODE
+        @hg = 1
+        ^^^^^^^ Instance variable `@hg` is too short.
+      CODE
+    end
+    specify 'one charactor instance variable also is marked as offensive' do
+      expect_offense <<~CODE
+        @a = 1
+        ^^^^^^ Instance variable `@a` is too short.
+      CODE
+    end
   end
 end
